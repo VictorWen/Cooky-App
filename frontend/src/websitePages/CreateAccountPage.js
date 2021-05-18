e4334aa9e0eb8809047cbed7f98a4db70e2fdb43
multixd <zainsfp@gmail.com>
2021-05-18 15:06:06 -0700

    added labels to CreateAccountPage.js

--- frontend/src/websitePages/CreateAccountPage.js
+++ frontend/src/websitePages/CreateAccountPage.js
@@ -1,9 +1,53 @@
 import React from 'react'
+import './CreateAccountPage.css'
 
 const CreateAccountPage = () => {
   return (
     <>
-      <h1>Create Account page</h1>
+      <body>
+        <h1>
+          Create an Account
+        </h1>
+
+        <div>
+        <label htmlFor="userName">User Name</label>
+        <input type="text"
+          id = "userName"
+          name = "userName"
+          placeholder= "Your User Name"
+        /> <br/>
+
+        <label htmlFor="passWord">Password</label>
+        <input type="text"
+          id = "passWord"
+          name = "passWord"
+          placeholder= "Your Password"
+        /> <br/>
+
+        <label htmlFor="firstName">First Name</label>
+        <input type="text"
+          id = "firstName"
+          name = "firstName"
+          placeholder= "Your First Name"
+        /> <br/>
+
+        <label htmlFor="lastName">Last Name</label>
+        <input type="text"
+          id = "lastName"
+          name = "lastName"
+          placeholder= "Your Last Name"
+        /> <br/>
+
+    <label htmlFor="country">Country</label>
+    <select id="country" name="country">
+      <option value="usa">USA</option>
+      <option value="unitedKingdom">United Kingdom</option>
+      <option value="canada">Canada</option>
+    </select>
+
+        </div>
+      </body>
+      
     </>
   )
 }
