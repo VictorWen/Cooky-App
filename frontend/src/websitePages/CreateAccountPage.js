4a299927607581bda8ae6747c52ce9bdc692bb0b
multixd <zainsfp@gmail.com>
2021-05-18 15:33:26 -0700

    Added submit button to create account

--- frontend/src/websitePages/CreateAccountPage.js
+++ frontend/src/websitePages/CreateAccountPage.js
@@ -10,42 +10,47 @@ const CreateAccountPage = () => {
         </h1>
 
         <div>
-        <label htmlFor="userName">User Name</label>
-        <input type="text"
-          id = "userName"
-          name = "userName"
-          placeholder= "Your User Name"
-        /> <br/>
+          <form>
 
-        <label htmlFor="passWord">Password</label>
-        <input type="text"
-          id = "passWord"
-          name = "passWord"
-          placeholder= "Your Password"
-        /> <br/>
+            <label htmlFor="userName">User Name</label>
+            <input type="text"
+              id = "userName"
+              name = "userName"
+              placeholder= "Your User Name"
+            /> <br/>
 
-        <label htmlFor="firstName">First Name</label>
-        <input type="text"
-          id = "firstName"
-          name = "firstName"
-          placeholder= "Your First Name"
-        /> <br/>
+            <label htmlFor="passWord">Password</label>
+            <input type="text"
+              id = "passWord"
+              name = "passWord"
+              placeholder= "Your Password"
+            /> <br/>
 
-        <label htmlFor="lastName">Last Name</label>
-        <input type="text"
-          id = "lastName"
-          name = "lastName"
-          placeholder= "Your Last Name"
-        /> <br/>
+            <label htmlFor="firstName">First Name</label>
+            <input type="text"
+              id = "firstName"
+              name = "firstName"
+              placeholder= "Your First Name"
+            /> <br/>
 
-    <label htmlFor="country">Country</label>
-    <select id="country" name="country">
-      <option value="usa">USA</option>
-      <option value="unitedKingdom">United Kingdom</option>
-      <option value="canada">Canada</option>
-    </select>
+            <label htmlFor="lastName">Last Name</label>
+            <input type="text"
+              id = "lastName"
+              name = "lastName"
+              placeholder= "Your Last Name"
+            /> <br/>
 
+            <label htmlFor="country">Country</label>
+            <select id="country" name="country">
+              <option value="usa">USA</option>
+              <option value="unitedKingdom">United Kingdom</option>
+              <option value="canada">Canada</option>
+            </select>
+            <input type="submit" value="Submit"></input>
+          </form>
+          
         </div>
+
       </body>
       
     </>
