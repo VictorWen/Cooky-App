d06d69e0654f203938bb867f1d7e19a520b9d464
Samarth Upadhyaya <samarthupadhyaya27@gmail.com>
2021-05-27 07:59:47 +0800

    Finished popular recipes page skeleton

--- frontend/src/websitePages/CreateAccountPage.js
+++ frontend/src/websitePages/CreateAccountPage.js
@@ -1,63 +1,86 @@
-import React from 'react'
+import React, {useState, useRef} from 'react'
 import styles from '../styles/CreateAccountPage.module.css'
 
 const CreateAccountPage = () => {
+  const [email, setEmail] = useState('')
+  const emailRef = useRef()
+  const [password, setPassword] = useState('')
+  const passwordRef = useRef()
+  const [passwordConfirmation, setPasswordConfirmation] = useState('')
+  const passwordConfirmationRef = useRef()
+
+  const handleSubmit = (event) => {
+    if (password === passwordConfirmation) {
+    }
+
+  }
+
   return (
-    <>
-        <h1>
-          Create an Account
-        </h1>
+        <div className={styles.container}>
+          <form className={styles.form}>
+            <div>
+              <h1 className={styles.signUpTitle}>Sign Up</h1>
+            </div>
+              <div>
+                <label htmlFor="email">Email:</label>
+                <input type="text"
+                       id="email"
+                       className={styles.userInput}
+                       name="email"
+                       value={email}
+                       onChange={(event) => {
+                         setEmail(event.target.value)
+                       }}
+                       placeholder="Enter your email..."
+                       required
+                /> <br/>
+              </div>
+            <div>
+              <label htmlFor="password">Password:</label>
+              <input type="password"
+                     id="password"
+                     className={styles.userInput}
+                     name="password"
+                     value={password}
+                     onChange={(event) => {
+                       setPassword(event.target.value)
+                     }}
+                     required
+              /> <br/>
+            </div>
 
-          <form>
+            <div>
+              <label htmlFor="passwordConfirmation">Confirm Password:</label>
+              <input type="password"
+                     id="passwordConfirmation"
+                     className={styles.userInput}
+                     name="passwordConfirmation"
+                     value={passwordConfirmation}
+                     onChange={(event) => {
+                       setPasswordConfirmation(event.target.value)
+                     }}
+                     required
+              /> <br/>
+            </div>
+            <div>
+              <button
+                name="signUpButton"
+                type="button"
+                value="Sign Up"
+                className={styles.signUpButton}
+                onClick={() => {
 
-            <label htmlFor="userName">User Name</label>
-            <input type="text"
-                   className={styles.userInput}
-              id = "userName"
-              name = "userName"
-              placeholder= "Your User Name"
-            /> <br/>
+                }}
+              > Sign Up
+              </button>
+            </div>
+            <br />
+            <div className={styles.signUpTitle}>
+              <p> I already have an account </p>
+            </div>
 
-            <label htmlFor="passWord">Password</label>
-            <input type="text"
-                   className={styles.userInput}
-              id = "passWord"
-              name = "passWord"
-              placeholder= "Your Password"
-            /> <br/>
-
-            <label htmlFor="firstName">First Name</label>
-            <input type="text"
-                   className={styles.userInput}
-              id = "firstName"
-              name = "firstName"
-              placeholder= "Your First Name"
-            /> <br/>
-
-            <label htmlFor="lastName">Last Name</label>
-            <input type="text"
-                   className={styles.userInput}
-              id = "lastName"
-              name = "lastName"
-              placeholder= "Your Last Name"
-            /> <br/>
-
-            <label htmlFor="country">Country</label>
-            <select id="country" name="country"
-                    className={styles.userInput}
-            >
-              <option value="usa">USA</option>
-              <option value="unitedKingdom">United Kingdom</option>
-              <option value="canada">Canada</option>
-            </select>
-            <input type="button"
-                   value="Create an account"
-                   className={styles.addIngredientButton}
-            />
           </form>
-
-
-    </>
+        </div>
   )
 }
 
