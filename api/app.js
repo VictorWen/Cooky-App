53a3ac086fc783e1bdfdfa9380375c9c4d396b82
Victor Wen <vicwen02@g.ucla.edu>
2021-05-27 21:57:49 -0400

    Added a new user test

--- api/app.js
+++ api/app.js
@@ -60,7 +60,7 @@ app.use('/newuser', new_account_router);
 
 // Run Tests
 require('./tests/test_recipe_database');
-//require('./tests/test_user_database');
+require('./tests/test_user_database');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
