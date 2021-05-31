da159d7588d6a4c7ba10b7a4c6fe778538996a50
multixd <zainsfp@gmail.com>
2021-05-31 13:56:30 -0700

    Reformated Your Recipe page and the way recipes are displayed

--- frontend/src/websitePages/YourRecipesPage.js
+++ frontend/src/websitePages/YourRecipesPage.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
+import styles from '../styles/YourRecipesPage.module.css'
+import JSONDATA from '../mockData.json'
 
 const YourRecipesPage = () => {
+    const [searchTerm,setSearchTerm] = useState('')
     return (
-      <>
-        <h1>Recipes</h1>
-        </>
+      <div className = {styles.container}>
+          <input type = "text" 
+                placeholder = "search" 
+                className = {styles.searchFormat}
+                onChange = {event => {setSearchTerm(event.target.value)
+                }}
+            >
+            </input>
+            {JSONDATA.filter((value) => {
+                if(searchTerm =="") {
+                    return value.user_name;
+                }
+                else if (value.user_name.toLowerCase().includes(searchTerm.toLowerCase())) {
+                    return value.user_name;
+                }
+
+            })
+                .map((val,key) => {
+                return <div className = {styles.recipeFormat}> 
+                        Recipe Name: {val.user_name} {'\n'}
+                        Recipe Creator: {val.first_name} {val.last_name} {'\n'}
+                        Date Created : {val.email}
+                </div>
+            })}
+      </div>
     )
   }
   
