6f8e39156acf360ac7e6b6950d524dc5de0b7c20
Samarth Upadhyaya <samarthupadhyaya27@gmail.com>
2021-06-01 03:40:41 +0800

    fixed more github messups

--- api/utils/recipe_database.js
+++ api/utils/recipe_database.js
@@ -8,106 +8,104 @@ const recipe_properties = ['name', 'description', 'ingredients', 'steps', 'cookt
 
 class RecipeDataLoader {
 
-    constructor() {
-        let firebase_app = firebase.app(); // Get default app
+  constructor() {
+    let firebase_app = firebase.app(); // Get default app
 
-        this.database = firebase_app.firestore();
-        this.recipes = this.database.collection('recipes');
+    this.database = firebase_app.firestore();
+    this.recipes = this.database.collection('recipes');
+  }
+
+  async getRecipe(recipe_id) {
+    let recipes = this.recipes;
+    return await new Promise(function (resolve) {
+      recipes.doc(recipe_id).get().then(function (snapshot) {
+        resolve(snapshot.data());
+      });
+    });
+  }
+
+  async hasRecipe(recipe_id) {
+    let recipes = this.recipes;
+    return await new Promise(function (resolve) {
+      recipes.doc(recipe_id).get().then(function (snapshot) {
+        resolve(snapshot.exists);
+      });
+    });
+  }
+
+  // recipes have additional "ingredients_list" field that strictly contains the names of all ingredients
+  async searchIngredient(target_ingredient) {
+    let recipes = this.recipes;
+    return await new Promise(function (resolve) {
+      recipes.where("ingredients_list", "array-contains", target_ingredient).get().then(function (snapshot) {
+        resolve(snapshot.docs.map(doc => doc.data()));
+      });
+    });
+  }
+
+  async hasIngredient(target_ingredient) {
+    let recipes = this.recipes;
+    return await new Promise(function (resolve) {
+      recipes.where("ingredients_list", "array-contains", target_ingredient).get().then(function (snapshot) {
+        resolve(!snapshot.empty);
+      });
+    });
+  }
+
+
+  async getPopularRecipes() {
+    let recipes = this.recipes;
+    return await new Promise(function (resolve) {
+      recipes.orderBy("n_ratings", "desc").get().then(function (snapshot) {
+        resolve(snapshot.docs.map(doc => {
+          let data = doc.data();
+          return (data.total_rating / data.n_ratings) >= 4 ? data : undefined
+        }).filter(data => data != undefined));
+      });
+    });
+  }
+
+  filterRecipeProperties(recipe_data) {
+    // Filters out properties from recipe_data that are not needed
+    // Taken from https://stackoverflow.com/questions/38750705/filter-object-properties-by-key-in-es6
+    let filtered_recipe = Object.keys(recipe_data)
+      .filter((key) => recipe_properties.includes(key))
+      .reduce(function (obj, key) {
+        obj[key] = recipe_data[key];
+        return obj;
+      }, {});
+
+    return filtered_recipe;
+  }
+
+  async addRecipe(recipe_data) {
+    let filtered_recipe = this.filterRecipeProperties(recipe_data);
+    let new_recipe = await this.recipes.add(filtered_recipe);
+    return new_recipe.id;
+  }
+
+  async updateRecipe(recipe_id, recipe_data) {
+    let filtered_recipe = this.filterRecipeProperties(recipe_data);
+    await this.recipes.doc(recipe_id).update(filtered_recipe);
+  }
+
+  async deleteRecipe(recipe_id) {
+    await this.recipes.doc(recipe_id).delete();
+  }
+
+  async addRating(recipe_id, user_id, rating) {
+    // Update recipe's average rating
+    let updated_recipe_fields = {
+      total_rating: firebase.firestore.FieldValue.increment(rating),
+      n_ratings: firebase.firestore.FieldValue.increment(1)
     }
+    await this.recipes.doc(recipe_id).update(updated_recipe_fields);
 
-    async getRecipe(recipe_id) {
-        let recipes = this.recipes;
-        return await new Promise(function (resolve) {
-            recipes.doc(recipe_id).get().then(function(snapshot) {
-                resolve(snapshot.data());
-            });
-        });
-    }
-
-    async hasRecipe(recipe_id) {
-        let recipes = this.recipes;
-        return await new Promise(function (resolve) {
-            recipes.doc(recipe_id).get().then(function(snapshot){
-                resolve(snapshot.exists);
-            });
-        });
-    }
-
-    // recipes have additional "ingredients_list" field that strictly contains the names of all ingredients
-    async searchIngredient(target_ingredient) {
-        let recipes = this.recipes;
-        return await new Promise(function (resolve) {
-            recipes.where("ingredients_list", "array-contains", target_ingredient).get().then(function(snapshot) {
-                resolve(snapshot.docs.map(doc => doc.data()));
-            });
-        });
-    }
-
-    async hasIngredient(target_ingredient) {
-        let recipes = this.recipes;
-        return await new Promise(function (resolve) {
-            recipes.where("ingredients_list", "array-contains", target_ingredient).get().then(function(snapshot) {
-                resolve(!snapshot.empty);
-            });
-        });
-    }
-
-
-    filterRecipeProperties(recipe_data) {
-=======
-    async getPopularRecipes() {
-        let recipes = this.recipes;
-        return await new Promise(function(resolve) {
-            recipes.orderBy("n_ratings", "desc").get().then(function(snapshot) {
-                resolve(snapshot.docs.map(doc => {
-                    let data = doc.data();
-                    return (data.total_rating / data.n_ratings) >= 4 ? data : undefined
-                }).filter(data => data != undefined));
-            });
-        });
-    }
-
-    filterRecipeProperties(recipe_data) {
-        // Filters out properties from recipe_data that are not needed
-        // Taken from https://stackoverflow.com/questions/38750705/filter-object-properties-by-key-in-es6
-        let filtered_recipe = Object.keys(recipe_data)
-            .filter((key) => recipe_properties.includes(key))
-            .reduce(function(obj, key) {
-                obj[key] = recipe_data[key];
-                return obj;
-            }, {});
-
-        return filtered_recipe;
-    }
-
-    async addRecipe(recipe_data){
-        let filtered_recipe = this.filterRecipeProperties(recipe_data);
-        let new_recipe = await this.recipes.add(filtered_recipe);
-        return new_recipe.id;
-    }
-
-    async updateRecipe(recipe_id, recipe_data) {
-        let filtered_recipe = this.filterRecipeProperties(recipe_data);
-        await this.recipes.doc(recipe_id).update(filtered_recipe);
-    }
-
-    async deleteRecipe(recipe_id) {
-        await this.recipes.doc(recipe_id).delete();
-    }
-
-    async addRating(recipe_id, user_id, rating) {
-        // Update recipe's average rating
-        let updated_recipe_fields = {
-            total_rating: firebase.firestore.FieldValue.increment(rating),
-            n_ratings: firebase.firestore.FieldValue.increment(1)
-        }
-        await this.recipes.doc(recipe_id).update(updated_recipe_fields);
-
-        // Update user's list of ratings
-        let new_rating = {}
-        new_rating['ratings.' + recipe_id] = rating
-        await user_data.users.doc(user_id).update(new_rating);
-    }
+    // Update user's list of ratings
+    let new_rating = {}
+    new_rating['ratings.' + recipe_id] = rating
+    await user_data.users.doc(user_id).update(new_rating);
+  }
 }
 
 module.exports = new RecipeDataLoader();
