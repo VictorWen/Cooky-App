3276dd088810e4b666df9932a24e2d4e71e79da5
Samarth Upadhyaya <samarthupadhyaya27@gmail.com>
2021-06-03 08:37:28 +0800

    some rarefactoring and your recipes overhaul

--- frontend/src/websitePages/YourRecipesPage.js
+++ frontend/src/websitePages/YourRecipesPage.js
@@ -3,43 +3,10 @@ import styles from '../styles/YourRecipesPage.module.css'
 import JSONDATA from '../mockData.json'
 import RecipeDisplay from './components/recipeDisplay'
 
-const YourRecipesPage2 = () => {
-  const [searchTerm, setSearchTerm] = useState('')
-  return (
-    <div className={styles.container}>
-      <input type="text"
-             placeholder="search"
-             className={styles.searchFormat}
-             onChange={event => {
-               setSearchTerm(event.target.value)
-             }}
-      >
-      </input>
-      {JSONDATA.filter((value) => {
-        if (searchTerm === "") {
-          return value.user_name;
-        } else if (value.user_name.toLowerCase().includes(searchTerm.toLowerCase())) {
-          return value.user_name;
-        }
-
-      })
-        .map((val, key) => {
-          return <div className={styles.recipeFormat}>
-            Recipe Name: {val.user_name} {'\n'}
-            Recipe Creator: {val.first_name} {val.last_name} {'\n'}
-            Date Created : {val.email}
-          </div>
-        })}
-    </div>
-  )
-}
-
-
 const YourRecipesPage = () => {
   const [searchTerm, setSearchTerm] = useState('')
-  let renderedResults = []
 
-  renderedResults = JSONDATA.filter(value => {
+  const renderedResults = JSONDATA.filter(value => {
     if (searchTerm === "") {
       return value.user_name
     } else if (value.user_name.toLowerCase().includes(searchTerm.toLowerCase())) {
