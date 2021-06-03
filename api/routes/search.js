9208f230de6150fe4162394fd9cce98ee5cf6e33
Victor Wen <vicwen02@g.ucla.edu>
2021-06-02 20:13:43 -0400

    Made search recipes only return the first ten results

--- api/routes/search.js
+++ api/routes/search.js
@@ -3,18 +3,23 @@ let recipe_data = require('../utils/recipe_database');
 let router = express.Router()
 
 router.get('/recipes/popular', async function(req, res) {
-    res.json(await recipe_data.getPopularRecipes());
+    let matching_recipes = await recipe_data.getPopularRecipes(); 
+    matching_recipes.splice(10);
+    res.json(matching_recipes);
 });
 
 router.get('/recipes/name/:name', async function(req, res) {
     let name = req.params.name;
-    res.json(await recipe_data.searchByName(name));
+    let matching_recipes = await recipe_data.searchByName(name);
+    matching_recipes.splice(10);
+    res.json(matching_recipes);
 });
 
-// GET http://localhost:3001/search/recipes/ingredients/name=Carrots
+// GET http://localhost:3001/search/recipes/ingredients/Carrots
 router.get('/recipes/ingredients/:name', async function(req, res) {
     let name = req.params.name;
     let matching_recipes = await recipe_data.searchByIngredients(name);
+    matching_recipes.splice(10);
     res.json(matching_recipes);
 });
 
